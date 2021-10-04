e6b44bc9a085b14a5f1a9523efe1cacb1e588cef
zrr4832 <zrr4832@rit.edu>
2021-10-04 16:57:35 -0400

    Added some cool command line argument integration and more file creation.

--- week2/dropafile.ps1
+++ week2/dropafile.ps1
@@ -1,3 +1,11 @@
-$path = "C:\Users\zacha\Desktop\newfile.txt"
-New-Item -Path $path -ItemType File
-"You have been HACKED" | Out-File -FilePath $path
+$DesktopPath = [Environment]::GetFolderPath("Desktop")
+$FileName = "Duckie File"
+$FullPath = $DeskTopPath + "\"+ $FileName
+
+$NumFiles = $args[0]
+$Message = $args[1]
+
+for ($i = 0; $i -lt $NumFiles; $i++){
+    # New-Item -Path ($FullPath + $i) -ItemType File
+    $Message | Out-File -FilePath ($FullPath + $i + ".txt")
+}
